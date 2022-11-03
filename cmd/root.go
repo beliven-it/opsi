485083e053001589db424cd1da01dcde2164f1e8
Fabrizio Gortani <fabrizio.gortani@beliven.com>
2022-11-03 12:33:12 +0100

    chore: update root description

--- cmd/root.go
+++ cmd/root.go
@@ -26,13 +26,8 @@ var Scripts embed.FS
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "opsi",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "The root command",
+	Long:  `The root command`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
