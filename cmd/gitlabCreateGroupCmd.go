bbcc4b9e9b8f3ca54198118074b8ecf5900c3ede
Fabrizio Gortani <fabrizio.gortani@beliven.com>
2023-03-31 16:16:39 +0200

    feat: add new group command

---
+++ cmd/gitlabCreateGroupCmd.go
@@ -0,0 +1,52 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	slugify "github.com/mozillazg/go-slugify"
+	"github.com/spf13/cobra"
+)
+
+var gitlabCreateGroupCmd = &cobra.Command{
+	Use:   "group {group_name}",
+	Args:  cobra.ExactArgs(1),
+	Short: "Create a Gitlab group",
+	Long:  "Create a Gitlab group",
+	Example: `
+  Create a group with name "research"
+  opsi gitlab create group research
+
+  ---
+
+  Create a group with name "development" but with path to "devs"
+  opsi gitlab create group development -p devs
+	`,
+	Run: func(cmd *cobra.Command, args []string) {
+		// Take the name of the group
+		name := args[0]
+
+		// Take the pathname from the flag
+		pathname, _ := cmd.Flags().GetString("path")
+
+		// If the pathname is not provided
+		// let the system slugify the name
+		if pathname == "" {
+			pathname = slugify.Slugify(name)
+		}
+
+		// Create subgroup
+		groupID, err := gitlab.CreateSubgroup(name, pathname, nil)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		fmt.Println("Created new group with ID", groupID)
+	},
+}
+
+func init() {
+	gitlabCreateCmd.AddCommand(gitlabCreateGroupCmd)
+	gitlabCreateGroupCmd.Flags().StringP("path", "p", "", "The slugify name for the group")
+}
